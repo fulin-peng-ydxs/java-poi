7e3780aee85d4826072c4845e13b6e0992157854
pengfulin <1162855925@qq.com>
2024-09-02 17:45:02 +0800

    -fix

---
+++ src/main/java/poi/excel/utils/basic/CollectionUtils.java
@@ -0,0 +1,31 @@
+package poi.excel.utils.basic;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * 集合工具类
+ *
+ * @author peng_fu_lin
+ * 2022-09-07 15:55
+ */
+public class CollectionUtils {
+
+
+    /**是否为空
+     * 2022/9/29 0029-14:00
+     * @author pengfulin
+    */
+    public static boolean isEmpty(Collection<?> collection){
+        return collection==null||collection.isEmpty();
+    }
+
+    public static <T> Collection<T> isEmptyOrDefault(Collection<T> collection){
+        return isEmpty(collection)?Collections.emptyList():collection;
+    }
+
+    public static boolean isNotEmpty(Collection<?> collection){
+        return !isEmpty(collection);
+    }
+
+}
